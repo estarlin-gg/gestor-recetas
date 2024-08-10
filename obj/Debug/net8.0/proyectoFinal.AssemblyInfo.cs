7d6fd84ba204c9b4b8ad6c4903f6ce00bc44b9c5
estarlin.gg <estarlingerman17@gmail.com>
2024-08-10 11:33:26 -0400

    gg

--- obj/Debug/net8.0/proyectoFinal.AssemblyInfo.cs
+++ obj/Debug/net8.0/proyectoFinal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyectoFinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc3b417dd257a051a1d9684922f87ec6080c592e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3333e94fc576e8c83b886b58f44cf6c80828288b")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyectoFinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyectoFinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
